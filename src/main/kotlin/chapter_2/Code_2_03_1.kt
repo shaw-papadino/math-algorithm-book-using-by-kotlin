be0746e2b2a8eedfc4728bca6d60aaaab630fcda
shoyaokayama <sho.edu.human@gmail.com>
2024-03-29 17:55:11 +0900

    add chapter 2-3

---
+++ src/main/kotlin/chapter_2/Code_2_03_1.kt
@@ -0,0 +1,18 @@
+package chapter_2
+
+object Code_2_03_1 {
+    var cnt = 1000
+    fun func1(): Int {
+        return 2021;
+    }
+    fun func2(pos: Int): Int {
+        cnt += 1
+        return cnt + pos
+    }
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(func1())
+        println(func2(500))
+        println(func2(500))
+    }
+}
