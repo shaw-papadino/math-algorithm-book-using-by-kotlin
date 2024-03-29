838bb5988ddcef4d96b700a2dc6cc55ddb11e368
shoyaokayama <sho.edu.human@gmail.com>
2024-03-29 09:17:18 +0900

    add problem for chapter 2-1

---
+++ src/main/kotlin/chapter_2/problem/Code_2_01_3.kt
@@ -0,0 +1,16 @@
+package chapter_2.problem
+
+import java.util.*
+
+object Code_2_01_3 {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sc = Scanner(System.`in`)
+
+        val a1 = sc.nextInt()
+        val a2 = sc.nextInt()
+        val a3 = sc.nextInt()
+
+        println(a1 * a2 * a3)
+    }
+}
